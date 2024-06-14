c4d509a653b1d46b5a9f6fb714407a2a42fca1f9
sseong <sseong@localhost.localdomain>
2024-06-14 03:29:50 -0400

    fix:signup수정

--- cores/NetCore.c
+++ cores/NetCore.c
@@ -195,8 +195,8 @@ void signup_service(epoll_net_core* server_ptr, task* task) {
     mysql_free_result(res);
     
     snprintf(query, sizeof(query), 
-             "INSERT INTO signup_req (login_id, password, name, phone, email) VALUES ('%s', UNHEX(SHA2('%s',255)), '%s', '%s', '%s')",
-             cJSON_GetStringValue(id_ptr), cJSON_GetStringValue(pw_ptr), cJSON_GetStringValue(name_ptr), cJSON_GetStringValue(phone_ptr), cJSON_GetStringValue(email_ptr));
+             "INSERT INTO signup_req (login_id, password, name, phone, email) VALUES ('%s', UNHEX(SHA2('%s',%d)), '%s', '%s', '%s')",
+             cJSON_GetStringValue(id_ptr), cJSON_GetStringValue(pw_ptr), SHA2_HASH_LENGTH, cJSON_GetStringValue(name_ptr), cJSON_GetStringValue(phone_ptr), cJSON_GetStringValue(email_ptr));
     printf("%s",query);
     if (mysql_query(conn->conn, query)) {
         fprintf(stderr, "INSERT failed: %s\n", mysql_error(conn->conn));
