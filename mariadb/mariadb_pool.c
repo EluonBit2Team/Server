e5ee1f976bcb766e5e3999817abc3aedc3dead55
sseong <sseong@localhost.localdomain>
2024-06-18 04:30:14 -0400

    bug:사인업 디버깅 코드 추가

--- mariadb/mariadb_pool.c
+++ mariadb/mariadb_pool.c
@@ -80,7 +80,7 @@ conn_t* get_conn(mariadb_conn_pool_t* pool)
     sem_wait(&pool->pool_sem);
     pthread_mutex_lock(&pool->pool_idx_mutex);
     int availableIdx = pool->pool_idx_stack[pool->pool_idx_stack_top];
-    printf("stackIdx:%ld, poolIdx:%ld, poolPtr:%p pop\n", pool->pool_idx_stack_top, availableIdx, &pool->pool[availableIdx].conn);
+    printf("top:%ld, top_idx:%ld, top:%p pop\n", pool->pool_idx_stack_top, pool->pool_idx_stack[pool->pool_idx_stack_top], pool->pool[pool->pool_idx_stack[pool->pool_idx_stack_top]].conn);
     --pool->pool_idx_stack_top;
     conn_t* rt = &pool->pool[availableIdx];
     pthread_mutex_unlock(&pool->pool_idx_mutex);
@@ -94,7 +94,7 @@ void release_conn(mariadb_conn_pool_t* pool, conn_t* conn)
     pthread_mutex_lock(&pool->pool_idx_mutex);
     ++pool->pool_idx_stack_top;
     pool->pool_idx_stack[pool->pool_idx_stack_top] = conn->idx;
-    printf("stackIdx:%ld, poolIdx:%ld, poolPtr:%p pop\n", pool->pool_idx_stack_top, pool->pool_idx_stack[pool->pool_idx_stack_top], &pool->pool[pool->pool_idx_stack[pool->pool_idx_stack_top]].conn);
+    printf("top:%ld, top_idx:%ld, top:%p release\n", pool->pool_idx_stack_top, pool->pool_idx_stack[pool->pool_idx_stack_top], pool->pool[pool->pool_idx_stack[pool->pool_idx_stack_top]].conn);
     pthread_mutex_unlock(&pool->pool_idx_mutex);
     sem_post(&pool->pool_sem);
 }
