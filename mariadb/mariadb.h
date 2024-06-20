18b5c7737a4be6c42b587387d50892ac4e91e12b
gyujun9403 <gujun112@naver.com>
2024-06-20 16:17:52 +0900

    style:db 구조 변경 적용

--- mariadb/mariadb.h
+++ mariadb/mariadb.h
@@ -9,15 +9,15 @@
 #define USER_SETTING_DB "user_setting_db"
 #define CHAT_GROUP_DB "chat_group_db"
 #define STATISTIC_DB "server_statistic_db"
-#define USER_REQUEST_DB "user_request_db"
+//#define USER_REQUEST_DB "user_request_db"
 #define LOG_DB "log_db"
 
-#define TOTAL_DB_NUM 5
+#define TOTAL_DB_NUM 4
 #define USER_SETTING_DB_IDX 0
 #define CHAT_GROUP_DB_IDX 1
 #define STATISTIC_DB_IDX 2
-#define USER_REQUEST_DB_IDX 3
-#define LOG_DB_IDX 4
+#define LOG_DB_IDX 3
+//#define USER_REQUEST_DB_IDX 3
 
 typedef struct chatdb {
     mariadb_conn_pool_t pools[TOTAL_DB_NUM];
