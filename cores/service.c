9c04f90fe7755612e5c022c653a05f433675dfce
sseong <sseong@localhost.localdomain>
2024-06-24 21:00:23 -0400

    fix: main merge

--- cores/service.c
+++ cores/service.c
@@ -605,14 +605,14 @@ void Mng_signup_approve_service(epoll_net_core* server_ptr, task_t* task) {
     int pos = cJSON_GetNumberValue(pos_ptr);
     int role = cJSON_GetNumberValue(role_ptr);
     int max_tps = cJSON_GetNumberValue(max_tps_ptr);
-    printf("%d %d %d %d\n",dept,pos,role,max_tps);
+
     snprintf(SQL_buf, sizeof(SQL_buf), "SELECT uid FROM user WHERE uid = %d AND role = 1", uid);
     query_result_to_bool(user_setting_conn, &msg, SQL_buf);
     if (msg != NULL) {
         goto cleanup_and_respond;
     }
     snprintf(SQL_buf, sizeof(SQL_buf), "SELECT login_id, password, name, phone, email FROM signup_req WHERE login_id = '%s'",cJSON_GetStringValue(id_ptr));
-    printf("%s\n",SQL_buf);
+
     cJSON* user_data_array = query_result_to_json(user_setting_conn, &msg, SQL_buf, 5, "login_id", "password", "name", "phone", "email");
     if (msg != NULL) {
         goto cleanup_and_respond;
