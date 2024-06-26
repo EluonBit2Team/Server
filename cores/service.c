6446f37c1cae1bc8b9667aacbe0ac0b741260805
sseong <sseong@localhost.localdomain>
2024-06-26 03:14:37 -0400

    fix:edit_user수정

--- cores/service.c
+++ cores/service.c
@@ -1139,7 +1139,7 @@ void edit_user_info_service(epoll_net_core* server_ptr, task_t* task) {
         msg = "user send invalid json. Miss dept";
         goto cleanup_and_respond;
     }
-    else if (cJSON_GetStringValue(dept_ptr)[0] != '\0') {
+    else if (cJSON_GetNumberValue(dept_ptr) != '\0') {
         printf("b\n");
         snprintf(SQL_buf, sizeof(SQL_buf), "UPDATE user SET did = %d WHERE login_id = '%s'",cJSON_GetNumberValue(dept_ptr),cJSON_GetStringValue(login_id_ptr));
         printf("c\n");
@@ -1157,7 +1157,7 @@ void edit_user_info_service(epoll_net_core* server_ptr, task_t* task) {
         msg = "user send invalid json. Miss pos";
         goto cleanup_and_respond;
     }
-    else if (cJSON_GetStringValue(pos_ptr)[0] != '\0') {
+    else if (cJSON_GetNumberValue(pos_ptr) != '\0') {
         printf("b\n");
         snprintf(SQL_buf, sizeof(SQL_buf), "UPDATE user SET position = %d WHERE login_id = '%s'",cJSON_GetNumberValue(pos_ptr),cJSON_GetStringValue(login_id_ptr));
         query_result_to_execuete(user_setting_conn, &msg, SQL_buf);
@@ -1173,7 +1173,7 @@ void edit_user_info_service(epoll_net_core* server_ptr, task_t* task) {
         msg = "user send invalid json. Miss role";
         goto cleanup_and_respond;
     }
-    else if (cJSON_GetStringValue(dept_ptr)[0] != '\0') {
+    else if (cJSON_GetNumberValue(dept_ptr) != '\0') {
         snprintf(SQL_buf, sizeof(SQL_buf), "UPDATE user SET role = %d WHERE login_id = '%s'",cJSON_GetNumberValue(role_ptr),cJSON_GetStringValue(login_id_ptr));
         query_result_to_execuete(user_setting_conn, &msg, SQL_buf);
         if (msg != NULL) {
@@ -1188,7 +1188,7 @@ void edit_user_info_service(epoll_net_core* server_ptr, task_t* task) {
         msg = "user send invalid json. Miss max_tps";
         goto cleanup_and_respond;
     }
-    else if (cJSON_GetStringValue(max_tps_ptr)[0] != '\0') {
+    else if (cJSON_GetNumberValue(max_tps_ptr) != '\0') {
         snprintf(SQL_buf, sizeof(SQL_buf), "UPDATE user SET max_tps = %d WHERE login_id = '%s'",cJSON_GetNumberValue(max_tps_ptr),cJSON_GetStringValue(login_id_ptr));
         query_result_to_execuete(user_setting_conn, &msg, SQL_buf);
         if (msg != NULL) {
