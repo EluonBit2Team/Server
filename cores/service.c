df08e162652bc03f08b2ee4108e57110e1d9fdd6
sseong <sseong@localhost.localdomain>
2024-07-02 05:03:43 -0400

    fix:pre_dm수정

--- cores/service.c
+++ cores/service.c
@@ -1749,7 +1749,7 @@ void pre_dm_log_service(epoll_net_core* server_ptr, task_t* task) {
     snprintf(SQL_buf, sizeof(SQL_buf), "SELECT sender_login_id, text, timestamp FROM dm_log WHERE (recver_uid = %d AND sender_uid = %d) OR \
     (recver_uid = %d AND sender_uid = %d) AND timestamp BETWEEN '%s' AND '%s' ORDER BY timestamp ASC",
     recver_uid,uid,uid,recver_uid,cJSON_GetStringValue(start_time_ptr),cJSON_GetStringValue(end_time_ptr));
-    cJSON* chat_log = query_result_to_json(log_conn, &msg, SQL_buf, 3, "login_id" ,"text", "timestamp");
+    cJSON* chat_log = query_result_to_json(log_conn, &msg, SQL_buf, 3, "sender_login_id" ,"text", "timestamp");
     if (msg != NULL) {
         goto cleanup_and_respond;
     }
