94e231ae61728bfa5bf933cda066d5f19884dc2f
sseong <sseong@localhost.localdomain>
2024-07-02 04:41:23 -0400

    fix:chat_in_user수정

--- cores/service.c
+++ cores/service.c
@@ -1673,7 +1673,7 @@ void chat_in_user_service(epoll_net_core* server_ptr, task_t* task) {
     printf("%d \n", recieve_fd); write(STDOUT_FILENO, task->buf, task->task_data_len); write(STDOUT_FILENO, "\n", 1);
     write(STDOUT_FILENO, "true:", 5); write(STDOUT_FILENO, task->buf + HEADER_SIZE, task->task_data_len - HEADER_SIZE); write(STDOUT_FILENO, "\n", 1);
     reserve_epoll_send(server_ptr->epoll_fd, session, response_str, strlen(response_str));
-    
+    reserve_epoll_send(server_ptr->epoll_fd, now_session, response_str, strlen(response_str));
 
 cleanup_and_respond:
     if (msg != NULL) {
