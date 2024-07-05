7483b5be04b9fa19096794d2205873c88ae1366d
gyujun9403 <gujun112@naver.com>
2024-07-05 13:00:10 +0900

    error:서버 다운 noti패킷 문제 해결 - sizeof/strlen착각

--- cores/service.c
+++ cores/service.c
@@ -1919,9 +1919,10 @@ void server_down_notice_to_all(epoll_net_core* server_ptr) {
     response_str = cJSON_Print(result_json);
     down_notice_total_size = HEADER_SIZE + strlen(response_str);
     memcpy(notice_send_buf, (char*)(&down_notice_total_size), HEADER_SIZE);
-    memcpy(notice_send_buf + HEADER_SIZE, response_str, sizeof(response_str));
-    printf("%d, %s\n", down_notice_total_size, response_str);
-    write(STDOUT_FILENO, "SEND:", 5); write(STDOUT_FILENO, notice_send_buf, down_notice_total_size); write(STDOUT_FILENO, "\n", 1);
+    strcat(notice_send_buf + HEADER_SIZE, response_str);
+    //memcpy(notice_send_buf + HEADER_SIZE, response_str, down_notice_total_size - HEADER_SIZE);
+    //printf("%d, %s, %s\n", down_notice_total_size, response_str, notice_send_buf + HEADER_SIZE);
+    //write(STDOUT_FILENO, "SEND:", 5); write(STDOUT_FILENO, notice_send_buf, down_notice_total_size); write(STDOUT_FILENO, "\n", 1);
     for (int i = 0; i < MAX_CLIENT_NUM; i++) {
         if (server_ptr->session_pool.session_pool[i].fd < 0) {
             continue ;
